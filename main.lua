36b25fc241c0a2be356a52be2df65c76704d5f94
gustavo1873 <guimaraesgustavo2003@gmail.com>
2023-09-28 20:03:48 -0300

    adding movement to paddles

--- main.lua
+++ main.lua
@@ -1,15 +1,18 @@
 push = require 'push'
 
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1080
 
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
+
+PADDLE_SPEED = 200
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     smallFont = love.graphics.newFont('font.ttf', 8)
+    scoreFont= love.graphics.newFont('font.ttf', 32)
 
     love.graphics.setFont(smallFont)
      
@@ -18,6 +21,30 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+end
+
+function love.update(dt)
+    --player 1 paddle movement
+    if love.keyboard.isDown('w') then
+        -- negative paddle speed because Y up is negative
+        player1Y = player1Y + -PADDLE_SPEED * dt
+        -- positive paddle speed because Y down is positive
+    elseif love.keyboard.isDown('s') then 
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end 
+
+    --player 2 paddle movement
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y + - PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end 
 end
 
 function love.keypressed(key)
@@ -29,10 +56,15 @@ end
 function love.draw()
     push:apply('start')
 
-   -- love.graphics.clear(40, 45, 52, 255)
+    love.graphics.clear(237, 231, 225, 0)
     
     love.graphics.printf('Welcome to Pong', 0, 20, VIRTUAL_WIDTH, 'center')
 
+    love.graphics.setFont(scoreFont)
+
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+    
     --left paddle
     love.graphics.rectangle('fill', 10, 30, 5, 20)
 
