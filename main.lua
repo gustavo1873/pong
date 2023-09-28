bb9ed4e031118bad93aa35156f8db3e54e596ad6
gustavo1873 <guimaraesgustavo2003@gmail.com>
2023-09-28 19:31:25 -0300

    drawing pong game

--- main.lua
+++ main.lua
@@ -7,10 +7,17 @@ VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
 function love.load()
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT{
+    love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    }
-)
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    love.graphics.setFont(smallFont)
+     
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
 end
 
 function love.keypressed(key)
@@ -22,7 +29,18 @@ end
 function love.draw()
     push:apply('start')
 
-    love.graphics.printf('Hello, Pong', 0, (VIRTUAL_HEIGHT / 2) - 6, VIRTUAL_WIDTH, 'center')
+   -- love.graphics.clear(40, 45, 52, 255)
+    
+    love.graphics.printf('Welcome to Pong', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    --left paddle
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+
+    --right paddle
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    
+    --ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
     
     push:apply('end')
 end
