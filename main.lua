05b3c16a1f55fd0a960ad2ea7d11248c12c70335
gustavo1873 <guimaraesgustavo2003@gmail.com>
2023-09-30 02:42:55 -0300

    added sound effects

--- main.lua
+++ main.lua
@@ -22,12 +22,16 @@ function love.load()
     smallFont = love.graphics.newFont('font.ttf', 8)
     largeFont = love.graphics.newFont('font.ttf', 16)
     scoreFont= love.graphics.newFont('font.ttf', 32)
-
-    love.graphics.setFont(smallFont)
-     
+   love.graphics.setFont(smallFont)
+   
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+    }
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false,
+        resizable = true,
         vsync = true
     })
 
@@ -66,6 +70,7 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+            sounds['paddle_hit']:play()
         end
         if ball:collides(player2) then
             ball.dx = -ball.dx * 1.05
@@ -76,23 +81,27 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+            sounds['paddle_hit']:play()
         end
 
        --this block detects if the ball hits the top or bottom boundaries of the screen and if so, it reverses the trajectory
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
         end
 
         if ball.y >= VIRTUAL_HEIGHT - 4 then
             ball.y = VIRTUAL_HEIGHT - 4
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
         end
 
 
         if ball.x < 0 then
             servingPlayer = 1
             player2Score = player2Score + 1
+            sounds['score']:play()
             if player2Score == 5 then
                 winningPlayer = 2
                 gameState = 'done'
@@ -105,6 +114,7 @@ function love.update(dt)
         if ball.x > VIRTUAL_WIDTH then
             servingPlayer = 2
             player1Score = player1Score + 1
+            sounds['score']:play()
             if player1Score == 5 then
                 winningPlayer = 1
                 gameState = 'done'
@@ -171,7 +181,7 @@ end
 function love.draw()
     push:apply('start')
 
-    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+    love.graphics.clear(0/255, 45/255, 52/255, 255/255)
 
     love.graphics.setFont(smallFont)
 
