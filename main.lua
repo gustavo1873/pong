1a791755e63a6a237a3430a0ed95f951e6fd0ca6
gustavo1873 <guimaraesgustavo2003@gmail.com>
2023-09-29 23:02:04 -0300

    OOP added

--- main.lua
+++ main.lua
@@ -1,4 +1,8 @@
 push = require 'push'
+Class = require 'class'
+require 'Paddle'
+require 'Ball'
+
 
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
@@ -6,7 +10,7 @@ WINDOW_HEIGHT = 1080
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
-PADDLE_SPEED = 200
+PADDLE_SPEED = 500
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -14,27 +18,24 @@ function love.load()
     math.randomseed(os.time())
 
     smallFont = love.graphics.newFont('font.ttf', 8)
-    scoreFont= love.graphics.newFont('font.ttf', 32)
+  --  scoreFont= love.graphics.newFont('font.ttf', 32)
 
     love.graphics.setFont(smallFont)
      
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false,
+        resizable = true,
         vsync = true
     })
 
    -- player1Score = 0
    -- player2Score = 0
 
-    player1Y = 30
-    player2Y = VIRTUAL_HEIGHT - 50
+   -- The numbers between parentheses are the parameters x, y, width, height that were set in the Paddle class
+    player1 = Paddle(10, 30, 5, 20)
+    player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
 
-    ballX = VIRTUAL_WIDTH / 2 - 2
-    ballY = VIRTUAL_HEIGHT / 2 - 2
-
-    ballDX = math.random(2) == 1 and 100 or -100
-    ballDY = math.random(-50, 50)
+    ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     gameState = 'start'
 end
@@ -43,23 +44,29 @@ function love.update(dt)
     --player 1 paddle movement
     if love.keyboard.isDown('w') then
         -- negative paddle speed because Y up is negative
-        player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)
+        player1.dy = -PADDLE_SPEED
         -- positive paddle speed because Y down is positive
     elseif love.keyboard.isDown('s') then 
-        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+        player1.dy = PADDLE_SPEED
+    else
+       player1.dy = 0
     end 
 
     --player 2 paddle movement
     if love.keyboard.isDown('up') then
-        player2Y = math.max(0, player2Y + -PADDLE_SPEED * dt)
+        player2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then
-        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
-    end 
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
+    end
+
 
     if gameState == 'play' then
-        ballX = ballX + ballDX * dt
-        ballY = ballY + ballDY * dt
+        ball:update(dt)
     end
+    player1:update(dt)
+    player2:update(dt)
 end
 
 function love.keypressed(key)
@@ -70,10 +77,7 @@ function love.keypressed(key)
             gameState = 'play'
         else
             gameState = 'start'
-            ballX = VIRTUAL_WIDTH / 2 - 2
-            ballY = VIRTUAL_HEIGHT / 2 - 2
-            ballDX = math.random(2) == 1 and 100 or -100
-            ballDY = math.random(-50, 50) * 1.5
+            ball:reset()
         end
     end
 end
@@ -82,22 +86,26 @@ function love.draw()
     push:apply('start')
 
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
-    
-    love.graphics.printf('Welcome to Pong', 0, 20, VIRTUAL_WIDTH, 'center')
 
+    love.graphics.setFont(smallFont)
+    
+    if gameState == 'start' then
+        love.graphics.printf('Press Enter to start', 0, 20, VIRTUAL_WIDTH, 'center')
+    else
+        love.graphics.printf('Play Pong!', 0, 20, VIRTUAL_WIDTH, 'center' )
+    end
+    
    -- love.graphics.setFont(scoreFont)
 
    -- love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
    --love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
     
     --left paddle
-    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
-
+    player1:render()
     --right paddle
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
-    
-    --ball
-    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
+    player2:render()
+    --rendering ball using the class' method
+    ball:render()
     
     push:apply('end')
 end
